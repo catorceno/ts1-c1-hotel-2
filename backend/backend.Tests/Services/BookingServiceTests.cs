156361c8409f230ef59c331a9fa129e402ad0b97
catorceno <camilacatorceno005@gmail.com>
2026-06-19 15:21:53 -0400

    test: registro exitoso

---
+++ backend/backend.Tests/Services/BookingServiceTests.cs
@@ -0,0 +1,138 @@
+using System.Data.Common;
+using AutoMapper;
+using Moq;
+
+namespace backend.Tests;
+
+[TestFixture]
+public class BookingServiceTests
+{
+    private Mock<IBookingRepository> _bookingRepoMock;
+    private Mock<IGuestBookingRepository> _guestBookingRepoMock;
+    private Mock<IGuestRepository> _guestRepoMock;
+
+    private Mock<IRoomService> _roomServiceMock;
+    private Mock<IGuestService> _guestServiceMock;
+    private Mock<IPaymentService> _paymentServiceMock;
+
+    private Mock<IMapper> _mapperMock;
+    private BookingService _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _bookingRepoMock = new Mock<IBookingRepository>();
+        _guestBookingRepoMock = new Mock<IGuestBookingRepository>();
+        _guestRepoMock = new Mock<IGuestRepository>();
+
+        _roomServiceMock = new Mock<IRoomService>();
+        _guestServiceMock = new Mock<IGuestService>();
+        _paymentServiceMock = new Mock<IPaymentService>();
+
+        _mapperMock = new Mock<IMapper>();
+        _service = new BookingService(
+            _bookingRepoMock.Object,
+            _guestBookingRepoMock.Object,
+            _guestRepoMock.Object,
+
+            _roomServiceMock.Object,
+            _guestServiceMock.Object,
+            _paymentServiceMock.Object,
+                
+            _mapperMock.Object
+        );
+    }
+
+    [Test]
+    public async Task CreateBooking_FechaValidaYHabitacionDisponible_Exitoso()
+    {
+        // Arrange
+        var bookingId = 100;
+        var guestId = 1;
+        var name = "Juan Marquez";
+        var ci = "8673020";
+        var phone = "77999910";
+        var roomId = 100;
+        var basePrice = 250m;
+        var startDate = new DateOnly(2026, 6, 1);
+        var endDate = new DateOnly(2026, 6, 5);
+        var guestDto = new CreateGuestDto
+        {
+            Name = name,
+            Ci = ci,
+            Phone = phone
+        };
+        var dto = new CreateBookingDto
+        {
+            StartDate = startDate,
+            EndDate = endDate,
+            RoomId = roomId,
+            Guests = new List<CreateGuestDto>{ guestDto }
+        };
+
+        var guestResponse = new GuestResponseDto
+        {
+            Id = guestId,
+            Name = name,
+            Ci = ci,
+            Phone = phone
+        };
+        var bookingCreated = new Booking
+        {
+            Id = bookingId,
+            RoomId = roomId,
+            StartDate = startDate,
+            EndDate = endDate,
+            Status = BookingStatus.Reserved
+        };
+        var guestBooking = new GuestBooking
+        {
+            GuestId = guestId,
+            BookingId = bookingId
+        };
+        var guestBookingCreated = new GuestBooking
+        {
+            Id = 1,
+            GuestId = guestId,
+            BookingId = bookingId
+        };
+        var roomResponse = new RoomResponseDto
+        {
+            Id = roomId,
+            BasePrice = basePrice,
+            Type = "Simple"
+        };
+        var paymentCreated = new Payment
+        {
+            Id = 1,
+            BookingId = bookingId,
+            PricePerNight = basePrice
+        };
+        var bookingResponse = new BookingResponseDto
+        {
+            Id = bookingId,
+            Status = BookingStatus.Reserved
+        };
+
+        _roomServiceMock.Setup(r => r.IsAvailable(roomId, startDate, endDate)).ReturnsAsync(true);
+        _guestServiceMock.Setup(g => g.CreateOrGet(guestDto)).ReturnsAsync(guestResponse);
+
+        _mapperMock.Setup(m => m.Map<Booking>(dto)).Returns(bookingCreated);
+
+        _bookingRepoMock.Setup(b => b.CreateAsync(bookingCreated)).ReturnsAsync(bookingCreated);
+        _guestBookingRepoMock.Setup(gb => gb.CreateAsync(guestBooking)).ReturnsAsync(guestBookingCreated);
+
+        _paymentServiceMock.Setup(p => p.CreatePaymentForBooking(bookingCreated)).ReturnsAsync(paymentCreated);
+        _roomServiceMock.Setup(r => r.GetDetailsById(roomId)).ReturnsAsync(roomResponse);
+
+        _mapperMock.Setup(m => m.Map<BookingResponseDto>(bookingCreated)).Returns(bookingResponse);
+
+        // Act
+        var result = await _service.CreateBooking(dto);
+
+        // Assert
+        Assert.That(result.Status, Is.EqualTo(BookingStatus.Reserved));
+        _paymentServiceMock.Verify(p => p.CreatePaymentForBooking(It.Is<Booking>(b => b.Status == BookingStatus.Reserved)), Times.Once);
+        _guestBookingRepoMock.Verify(gb => gb.CreateAsync(It.Is<GuestBooking>(gb => gb.GuestId == guestResponse.Id)), Times.Once);
+    }
+}
--- backend/backend.Tests/obj/Debug/net8.0/backend.Tests.AssemblyInfo.cs
+++ backend/backend.Tests/obj/Debug/net8.0/backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a9a535a8cb7ec3427eac1880e831c64a0196e11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da0bfaec3c4ce6681098f9d74cdadbab156aa52")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
